8a2d83050226ad30ac80fde0a07f7319214f2a2a
jblandonsv <blandon.jaime@gmail.com>
2022-05-07 08:44:31 -0600

    Arreglo Temporal al Linting de Python para Nueva Version

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       +(python +lsp)      ; Esta configuracion empezo a fallar por eso la siguiente linea
+       ;; +(python +lsp)      ; Esta configuracion empezo a fallar por eso la siguiente linea
        +(python +lsp-pyright)  ; no dejar que instale mspyls (si me ha funcionado)?? necesario instalar los lsp server manualmente pip install --user "python-language-server[all]"
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
